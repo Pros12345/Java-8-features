47ab1367915702de3da61c76ebd4ece8e85949c2
Prosenjit <86827153+Pros12345@users.noreply.github.com>
2026-08-04 13:02:17 +0530

    Update FlatMap.java

--- FlatMap.java
+++ FlatMap.java
@@ -8,8 +8,6 @@ public class FlatMap {
 	public static void main(String[] args) {
 		List<List<String>> listOfList = Arrays.asList(Arrays.asList("a", "b"), Arrays.asList("c", "d"),
 				Arrays.asList("e", "f"));
-//		List<String> flatList = 
 		listOfList.stream().flatMap(list -> list.stream()).forEach(System.out::println);
-		// System.out.println(flatList);
 	}
 }
